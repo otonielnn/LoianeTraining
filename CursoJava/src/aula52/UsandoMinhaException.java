5e7c8c8638ac919878dcd751e1b98bbb192c3ab4
otonielnn <otoniel_2002@live.com>
2024-05-26 16:47:31 -0300

    Finalizando aula 52

---
+++ CursoJava/src/aula52/UsandoMinhaException.java
@@ -0,0 +1,30 @@
+package aula52;
+
+public class UsandoMinhaException {
+    public static void main(String[] args) {
+
+        try {
+            teste();
+        } catch (DivisaoNaoExata e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void teste() throws DivisaoNaoExata{
+        int[] numeros = { 4, 8, 5, 16, 32, 21, 64, 128 };
+        int[] denominador = { 2, 0, 4, 8, 0, 2, 3};
+
+        for (int i = 0; i < numeros.length; i++) {
+            try {
+                if (numeros[i] % 2 != 0) {
+                    throw new DivisaoNaoExata(numeros[i], denominador[i]);
+                }
+                System.out.println(numeros[i] + " / " + denominador[i] + " = " + numeros[i] / denominador[i]);
+            } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+                System.out.println("Aconteceu um erro.");
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
